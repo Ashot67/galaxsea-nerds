f1418a7a073107472d0f809cd73261032fa0b109
galaxsea <lunardunes@gmail.com>
2016-02-22 01:39:04 +0200

    Проверка итогового проекта

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,50 @@
-var link=document.querySelector(".write-us"),popup=document.querySelector(".modal-content"),close=popup.querySelector(".modal-content-close"),form=popup.querySelector("form"),name=popup.querySelector("[name=name]"),email=popup.querySelector("[name=email]"),storage=localStorage.getItem("name");link.addEventListener("click",function(a){a.preventDefault(),popup.classList.add("modal-content-show"),storage?(name.value=storage,email.focus()):name.focus()}),close.addEventListener("click",function(a){a.preventDefault(),popup.classList.remove("modal-content-show"),popup.classList.remove("modal-error")}),form.addEventListener("submit",function(a){name.value&&email.value?localStorage.setItem("name",name.value):(a.preventDefault(),popup.classList.remove("modal-error"),popup.offsetWidth=popup.offsetWidth,popup.classList.add("modal-error"))}),window.addEventListener("keydown",function(a){27===a.keyCode&&popup.classList.contains("modal-content-show")&&(popup.classList.remove("modal-content-show"),popup.classList.remove("modal-error"))});
+var link = document.querySelector(".write-us");
+
+      var popup = document.querySelector(".modal-content");
+      var close = popup.querySelector(".modal-content-close");
+
+      var form = popup.querySelector("form");
+      var login = popup.querySelector("[name=name]");
+      var password = popup.querySelector("[name=email]");
+
+      var storage = localStorage.getItem(".write-us");
+
+      link.addEventListener("click", function(event) {
+        event.preventDefault();
+        popup.classList.add("modal-content-show");
+
+        if (storage) {
+          name.value = storage;
+          email.focus();
+        } else {
+          name.focus();
+        }
+
+      });
+
+      close.addEventListener("click", function(event) {
+        event.preventDefault();
+        popup.classList.remove("modal-content-show");
+        popup.classList.remove("modal-error");
+      });
+
+      form.addEventListener("submit", function(event) {
+        if (!name.value || !email.value) {
+          event.preventDefault();
+          popup.classList.remove("modal-error");
+          popup.offsetWidth = popup.offsetWidth;
+          popup.classList.add("modal-error");
+        } else {
+          localStorage.setItem("name", name.value);
+        }
+      });
+
+      window.addEventListener("keydown", function(event) {
+        if (event.keyCode === 27) {
+          if (popup.classList.contains("modal-content-show")) {
+            popup.classList.remove("modal-content-show");
+            popup.classList.remove("modal-error");
+          }
+        }
+      });
+      
